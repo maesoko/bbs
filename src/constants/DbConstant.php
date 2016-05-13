33efff385c35ff67fa12f1488bedd82b5f47f5ca
maesoko <maesoko@jupit.co.jp>
2016-05-13 11:44:13 +0900

    [add] DbConstant.php

---
+++ src/constants/DbConstant.php
@@ -0,0 +1,8 @@
+<?php
+
+class DbConstant {
+    const DATABASE = 'db_bbs';
+    const USERNAME = 'maesoko';
+    const PASSWORD = 'fuga';
+    const DSN = 'mysql:dbhost=localhost;dbname=' . self::DATABASE;
+}
