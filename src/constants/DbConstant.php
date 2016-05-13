8a673be44fa250d12908f355704ecab25a167c95
maesoko <maesoko@jupit.co.jp>
2016-05-13 12:40:12 +0900

    [modify] クラス内定数の定義でエラーが発生していたので、定数の取得をゲッターに置き換えて対応した

--- src/constants/DbConstant.php
+++ src/constants/DbConstant.php
@@ -1,8 +1,25 @@
 <?php
 
+
 class DbConstant {
-    const DATABASE = 'db_bbs';
-    const USERNAME = 'maesoko';
-    const PASSWORD = 'fuga';
-    const DSN = 'mysql:dbhost=localhost;dbname=' . self::DATABASE;
+    /** ▼▼▼ データベース情報 ▼▼▼ */
+    const DSN_PREFIX = "mysql";
+    const DB_HOST = 'localhost';
+    const DB_NAME = 'db_bbs';
+    
+    /** ▼▼▼ 接続ユーザー情報 ▼▼▼ */
+    const DB_USERNAME = 'maesoko';
+    const DB_PASSWORD = 'fuga';
+
+    /**
+     * クラス内定数で文字列の結合ができないので、ゲッターで置き換えた。
+     * >> const DSN = 'mysql:dbhost=localhost;dbname=' . self::DB_NAME; < これがエラーになる。
+     * @return string DSNを文字列で返す
+     */
+    public static function getDSN() {
+        return self::DSN_PREFIX . ':'
+            . 'dbhost=' . self::DB_HOST . ';'
+            . 'dbname=' . self::DB_NAME;
+    }
 }
+
