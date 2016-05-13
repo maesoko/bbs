a7d611328d0094a2cf796872fc72eb1e7f70e9d5
maesoko <maesoko@jupit.co.jp>
2016-05-13 16:01:51 +0900

    [add] ThreadListView.php

---
+++ src/view/ThreadListView.php
@@ -0,0 +1,31 @@
+<?php
+require_once('BaseView.php');
+require_once(dirname(__FILE__) . './../dao/BbsThreadDao.php');
+
+class ThreadListView extends BaseView {
+
+    /**
+     * ThreadListView constructor.
+     */
+    public function __construct() {
+        parent::__construct(new BbsThreadDao());
+    }
+
+    /**
+     * @return string スレッド一覧の情報を<tr>,<td>タグで囲んだ文字列で返す。スレッドが存在しない場合は空文字を返す。
+     */
+    public function showThreadList() {
+        $result = "";
+        $threadList = $this->dao->getAllThreads();
+        foreach ($threadList as $thread) {
+            $result .= "<tr>";
+            $result .= "<td>{$thread->getId()}</td>";
+            $result .= "<td>{$thread->getTitle()}</td>";
+            $result .= "<td>{$thread->getComments()}</td>";
+            $result .= "<td>{$thread->getCreationDate()}</td>";
+            $result .= "</tr>" . PHP_EOL;
+        }
+
+        return $result;
+    }
+}
