2d7896381c6b6e767a3cd4368ee439b92e6f36aa
maesoko <maesoko@jupit.co.jp>
2016-05-16 20:45:12 +0900

    [modify] 不要なrequire_onceを削除

--- src/view/CreateThreadView.php
+++ src/view/CreateThreadView.php
@@ -1,8 +1,6 @@
 <?php
 require_once('BaseView.php');
 require_once(dirname(__FILE__) . './../dao/BbsThreadDao.php');
-require_once(dirname(__FILE__) . './../model/BbsResponse.php');
-require_once(dirname(__FILE__) . './../dao/BbsResponseDao.php');
 
 class CreateThreadView extends BaseView {
 
