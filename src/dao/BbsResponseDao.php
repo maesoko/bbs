7e2285b348318fdc45ea89b8a25898766f10cbac
maesoko <maesoko@jupit.co.jp>
2016-05-17 16:43:31 +0900

    [modify] スレッドのレスポンス一覧を取得するgetAllResponseByThreadId($threadId)を実装

--- src/dao/BbsResponseDao.php
+++ src/dao/BbsResponseDao.php
@@ -39,4 +39,26 @@ class BbsResponseDao extends BaseDao {
             $record['id'], $record['thread_id'], $record['comment_number'], $record['comment'],
             $record['name'], $record['mail_address'], $record['write_date']);
     }
+
+    /**
+     * スレッドのレスポンス一覧を取得する
+     * @param $threadId int 取得するスレッドのID
+     * @return array|null レスポンスの情報が入った配列を返す。|レスポンスが存在しない場合はnullを返す。
+     */
+    public function getAllResponseByThreadId($threadId) {
+        $sql = "SELECT * FROM response WHERE thread_id = :threadId";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindParam(':threadId', $threadId, PDO::PARAM_INT);
+        $stmt->execute();
+
+        $results = null;
+        $records = $stmt->fetchAll();
+        foreach ($records as $record) {
+            $results[] = new BbsResponse($record['id'], $record['thread_id'], 
+                $record['comment_number'], $record['comment'],
+                $record['name'], $record['mail_address'], $record['write_date']);
+        }
+        
+        return $results;
+    }
 }
