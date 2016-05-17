317896c435617bf5444ad34596e8773d9a8d1870
maesoko <maesoko@jupit.co.jp>
2016-05-17 12:40:16 +0900

    [add] Thread.php

---
+++ Thread.php
@@ -0,0 +1,20 @@
+<?php
+$threadId = null;
+if (isset($_GET['thread-id'])) {
+    $threadId = $_GET['thread-id'];
+}
+
+echo $threadId;
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>JUPIT BBS</title>
+
+    <link rel="stylesheet" href="css/common.css">
+</head>
+<body>
+</body>
+</html>
