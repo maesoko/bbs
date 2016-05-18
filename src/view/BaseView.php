3837f81b3ae40e6bbe9f204fec7dad59e6a15df3
maesoko <maesoko@jupit.co.jp>
2016-05-18 17:31:25 +0900

    [add] BaseView.php

---
+++ src/view/BaseView.php
@@ -0,0 +1,11 @@
+<?php
+
+abstract class BaseView {
+
+    /**
+     * 総レコード数を取得する
+     * @return int Viewで表示している件数ではなく、テーブルに保存されている総レコード数。int型にキャストして返却。
+     */
+    protected abstract function getRowCount();
+
+}
