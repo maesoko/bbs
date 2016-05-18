e2e8de748f7f7ce0c6f95ea8845484c4328e9dd5
maesoko <maesoko@jupit.co.jp>
2016-05-18 15:43:39 +0900

    [modify] 指定された件数分のスレッドを取得するgetThreadInLimit($limit, $offset)関数を実装した

--- src/dao/BbsThreadDao.php
+++ src/dao/BbsThreadDao.php
@@ -36,6 +36,38 @@ class BbsThreadDao extends BaseDao {
     }
 
     /**
+     * $limitで指定された件数分のスレッドを取得する
+     * @param $limit int 取得するスレッドの件数
+     * @param $offset int 取得するスレッドの開始位置
+     * @return array|null スレッド一覧の情報が入ったBbsThreadListクラスの配列を返す。スレッドが存在しない場合はnullを返す。
+     */
+    public function getThreadInLimit($limit, $offset) {
+        $sql = 'SELECT t.id, t.title, COUNT(r.thread_id) "comments", t.creation_date
+                FROM thread t LEFT JOIN response r
+                ON t.id = r.thread_id
+                GROUP BY t.id
+                LIMIT :limit OFFSET :offset';
+        
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindParam(':limit', $limit, PDO::PARAM_INT);
+        $stmt->bindParam(':offset', $offset, PDO::PARAM_INT);
+        $stmt->execute();
+
+        $result = null;
+        $records = $stmt->fetchAll();
+        foreach ($records as $record) {
+            $result[] = new BbsThreadList(
+                $record["id"],
+                $record["title"],
+                $record["creation_date"],
+                $record["comments"]
+            );
+        }
+
+        return $result;
+    }
+
+    /**
      * $titleで指定されたタイトル名でスレッドを新規作成する
      * @param $title string 新規作成するスレッドのタイトル
      * @return BbsThread 追加したスレッドの情報が入ったBbsThreadクラスのインスタントを返す。
