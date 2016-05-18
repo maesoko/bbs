bd476a0cbeabf26f7a28c6032ded316fb3534c2c
maesoko <maesoko@jupit.co.jp>
2016-05-18 14:15:12 +0900

    [delete] BaseView.php

--- src/view/CreateThreadView.php
+++ src/view/CreateThreadView.php
@@ -1,5 +1,4 @@
 <?php
-require_once('BaseView.php');
 require_once(dirname(__FILE__) . './../dao/BbsThreadDao.php');
 require_once(dirname(__FILE__) . './../dao/BbsResponseDao.php');
 
--- src/view/ThreadListView.php
+++ src/view/ThreadListView.php
@@ -1,5 +1,4 @@
 <?php
-require_once('BaseView.php');
 require_once(dirname(__FILE__) . './../dao/BbsThreadDao.php');
 
 class ThreadListView {
